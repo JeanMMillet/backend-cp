a07ea0144ac5872ce278f62d0f501390cef8db2b
JeanMMillet <millet.jm@live.com>
2024-04-15 14:58:49 +0200

    add countryTable in resetDb

--- src/resetDb.ts
+++ src/resetDb.ts
@@ -4,9 +4,10 @@ import { countries } from "./fixtures/fakeData";
 
 async function main() {
   await db.initialize();
-  await db.getRepository(Country).clear();
+  const countryTable = db.getRepository(Country);
+  await countryTable.clear();
   db.query("delete from sqlite_sequence where name='country';");
-  await db.getRepository(Country).save(countries);
+  await countryTable.save(countries);
 
   console.log("Database initialized ! 🚀🚀");
 }
